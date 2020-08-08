3939ec16d1555f1f0201c355d0e292b9cd8296ce
ijk <ijkten@no-email.com>
2020-08-08 11:58:08 -0400

    use effect

--- src/components/Home/MovieSearch.js
+++ src/components/Home/MovieSearch.js
@@ -1,4 +1,4 @@
-import React, {Fragment, useState} from 'react';
+import React, {Fragment, useState, useEffect} from 'react';
 import MovieSearchForm from './MovieSearchForm';
 
 const MovieSearch = () => {
@@ -6,8 +6,32 @@ const MovieSearch = () => {
   const initialState = '';
   const [query, setQuery] = useState(initialState);
 
+  const buildUrl = (query, page = 1) => {
+    return `https://api.themoviedb.org/3/search/movie?api_key=ff22151cbe2f68c90e1d8448c9923ad6&language=en-US&query=${query}&page=${page}&include_adult=false`
+  }
+
+  useEffect(() => {
+    if (query.length === 0) {
+      return;
+    }
+    
+    const searchMovies = async () => {
+      const response = await fetch(buildUrl(query));
+      const data = await response.json();
+      console.log(data);
+    }
+    
+    searchMovies();
+
+  }, [query]);
+
   const searchMovie = (e) => {
     setQuery(e.target.value);
+  }
+
+  const formSubmitted = (e) => {
+    e.preventDefault();
+    console.log('form submitted');
   }
 
   return (
@@ -15,6 +39,7 @@ const MovieSearch = () => {
       <MovieSearchForm 
         query={query}
         searchMovie={searchMovie}
+        formSubmitted={formSubmitted}
       />
     </Fragment>
   )
--- src/components/Home/MovieSearchForm.js
+++ src/components/Home/MovieSearchForm.js
@@ -1,22 +1,24 @@
 import React, {Fragment} from 'react';
 
+// ES6 syntax
 const MovieSearchForm = (props) => {
   return (
-    <Fragment>
-      <label htmlFor='movieSearchQuery'>Movie Search</label>
-      <input 
-        type='text' 
-        name='movieSearchQuery' 
-        id='movieSearchQuery'
-        onChange={(e) => props.searchMovie(e)}
-        value={props.query}
-      >
-      </input>
-      <button 
-        type='submit'>
-          Search
-      </button>
-    </Fragment>
+    <form 
+    onSubmit={(e) => props.formSubmitted(e)}>
+    <label htmlFor='movieSearchQuery'>Movie Search</label>
+    <input 
+      type='text' 
+      name='movieSearchQuery' 
+      id='movieSearchQuery'
+      onChange={(e) => props.searchMovie(e)}
+      value={props.query}
+    >
+    </input>
+    <button 
+      type='submit'>
+        Search
+    </button>
+  </form> 
   )
 }
 
